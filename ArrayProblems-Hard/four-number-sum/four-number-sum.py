a49d3976ed8b0310a90eb556a13492e331969d8f
belisky <denoblesnobility2@gmail.com>
2023-05-14 20:51:52 +0000

    chore: adds four-number-sum python problem solution

---
+++ ArrayProblems-Hard/four-number-sum/four-number-sum.py
@@ -0,0 +1,19 @@
+def fourNumberSum(array, targetSum):
+    initSums={}
+    quad=[]
+    for i in range(1,len(array)-1):
+        for j in range(i+1,len(array)):
+            curSum=array[i]+array[j]
+            diff=targetSum-curSum
+            if diff in initSums:
+                for duo in initSums[diff]:
+                    quad.append([*duo,array[i],array[j]])
+        for k in range(0,i):
+            curSum=array[i]+array[k]
+            if curSum not in initSums:
+                initSums[curSum]=[[array[i],array[k]]]
+            else:
+                initSums[curSum].append([array[i],array[k]])
+    return quad
+    # Write your code here.
+    pass
