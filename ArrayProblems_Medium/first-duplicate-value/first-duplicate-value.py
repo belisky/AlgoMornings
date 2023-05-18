e1327d808b7bf406943586e9896e956e986b9c23
belisky <denoblesnobility2@gmail.com>
2023-05-18 11:50:10 +0000

    chore: updates first-duplicate-value python problem solution

--- ArrayProblems_Medium/first-duplicate-value/first-duplicate-value.py
+++ ArrayProblems_Medium/first-duplicate-value/first-duplicate-value.py
@@ -1,20 +1,21 @@
 def firstDuplicateValue(array):
-    # Write your code here.
-    
-    max_count = {}
-    for _ , value in enumerate(array):
-        if value  in max_count:
-            max_count[value] += 1
-            return value 
-        else:
-            max_count[value] = 1
-            
+    for value in array:
+        absoluteValue=abs(value)
+        if array[absoluteValue-1]<0:
+            return absoluteValue
+        array[absoluteValue-1]*=-1
     return -1
+     
+
 # Solution 2
 # def firstDuplicateValue(array):
-#     for value in array:
-#         absVal=abs(value)
-#         if array[absVal-1]<0:
-#             return absVal
-#         array[absVal-1]*=-1
+#     max_count = {}
+#     for _ , value in enumerate(array):
+#         if value  in max_count:
+#             max_count[value] += 1
+#             return value 
+#         else:
+#             max_count[value] = 1
+            
 #     return -1
+
