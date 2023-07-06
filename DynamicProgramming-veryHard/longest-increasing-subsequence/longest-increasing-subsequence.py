93cb856f39ddce1d4e4e3113bc014935d60ee89d
belisky <denoblesnobility2@gmail.com>
2023-07-06 17:39:19 +0000

    updates python code

--- DynamicProgramming-veryHard/longest-increasing-subsequence/longest-increasing-subsequence.py
+++ DynamicProgramming-veryHard/longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,23 +1,25 @@
 def longestIncreasingSubsequence(array):
-    seq=[None for _ in array]
-    lengths=[1 for _ in array]
-    maxLen=0
+    seq = [None for _ in array]
+    lengths = [1 for _ in array]
+    maxLen = 0
     for i in range(len(array)):
-        cur=array[i]
-        for j in range(0,i):
-            other=array[j]
-            if other<cur and lengths[j]+1>=lengths[i]:
-                lengths[i]=lengths[j]+1
-                seq[i]=j
-        if lengths[i]>=lengths[maxLen]:
-            maxLen=i
-    return buildSeq(array,seq,maxLen)
-def buildSeq(array,seq,curIdx):
-    sequence=[]
+        cur = array[i]
+        for j in range(0, i):
+            other = array[j]
+            if other < cur and lengths[j]+1 >= lengths[i]:
+                lengths[i] = lengths[j]+1
+                seq[i] = j
+        if lengths[i] >= lengths[maxLen]:
+            maxLen = i
+    return buildSeq(array, seq, maxLen)
+
+
+def buildSeq(array, seq, curIdx):
+    sequence = []
     while curIdx is not None:
         sequence.append(array[curIdx])
-        curIdx=seq[curIdx]
+        curIdx = seq[curIdx]
     return list(reversed(sequence))
 
-                # Write your code here.
+    # Write your code here.
     pass
