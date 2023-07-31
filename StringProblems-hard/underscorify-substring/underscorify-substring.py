33a05468df8180a09b472b75047e961febcab3ba
belisky <denoblesnobility2@gmail.com>
2023-07-31 09:18:58 +0000

    update: adds a second commented out solution to underscorify substring

--- StringProblems-hard/underscorify-substring/underscorify-substring.py
+++ StringProblems-hard/underscorify-substring/underscorify-substring.py
@@ -27,4 +27,61 @@ def underscorifySubstring(string, substring):
             
     return under        
     # Write your code here.
+
+
+
+#Solution 2
+# def underscorifySubstring(string, substring):
+#     locations=collapse(getLocations(string,substring))
+#     return underscore(string,locations)
+
+# def getLocations(string,substring):
+#     locations=[]
+#     start=0
+#     while start<len(string):
+#         pos=string.find(substring,start)
+#         if pos!=-1:
+#             locations.append([pos,pos+len(substring)])
+#             start=pos+1
+#         else:
+#             break
+#     return locations
+
+# def collapse(locations):
+#     if not len(locations):
+#         return locations
+#     newLoc=[locations[0]]
+#     prev=newLoc[0]
+#     for i in range(1,len(locations)):
+#         cur=locations[i]
+#         if cur[0]<=prev[1]:
+#             prev[1]=cur[1]
+#         else:
+#             newLoc.append(cur)
+#             prev=cur
+#     return newLoc
+
+# def underscore(string,locations):
+#     underscoredString=[]
+#     isInMiddle=False
+#     start=0
+#     locCounter=0
+#     one_or_two=0
+#     while locCounter<len(locations) and start<len(string):
+#         if  start==locations[locCounter][one_or_two]:
+#             underscoredString.append('_')
+#             isInMiddle=not isInMiddle            
+#             if not isInMiddle:
+#                 locCounter+=1
+#             one_or_two=0 if one_or_two==1 else 1
+#         underscoredString.append(string[start])
+#         start+=1
+#     if locCounter<len(locations):
+#         underscoredString.append('_')
+#     elif start<len(string):
+#         underscoredString.append(string[start:])
+#     return ''.join(underscoredString)
+#     # Write your code here.
+    
+
    
