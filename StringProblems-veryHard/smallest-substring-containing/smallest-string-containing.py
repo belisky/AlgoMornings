f44543d04d01e317571f56e629815bae93438ea5
belisky <denoblesnobility2@gmail.com>
2023-08-03 08:30:06 +0000

    updates to smallest-substring-containing

--- StringProblems-veryHard/smallest-substring-containing/smallest-string-containing.py
+++ StringProblems-veryHard/smallest-substring-containing/smallest-string-containing.py
@@ -1,61 +1,67 @@
 def smallestSubstringContaining(bigString, smallString):
-    smallStringFrequency=getCharCounts(smallString)
-    subBounds=getStringBounds(smallStringFrequency,bigString)
-    charString=getStringFromBounds(bigString,subBounds)
+    smallStringFrequency = getCharCounts(smallString)
+    subBounds = getStringBounds(smallStringFrequency, bigString)
+    charString = getStringFromBounds(bigString, subBounds)
     return charString
-    # Write your code here.
-  
-def getStringBounds(target,string):
-    subStringBounds=[0,float('inf')]
-    subStringChar={}
-    numUnique=len(target.keys())
-    numUniqueDone=0
-    leftIdx=0
-    rightIdx=0
-    while rightIdx<len(string):
-        rightCh=string[rightIdx]
+
+
+def getStringBounds(target, string):
+    subStringBounds = [0, float('inf')]
+    subStringChar = {}
+    numUnique = len(target.keys())
+    numUniqueDone = 0
+    leftIdx = 0
+    rightIdx = 0
+    while rightIdx < len(string):
+        rightCh = string[rightIdx]
         if rightCh not in target:
-            rightIdx+=1
+            rightIdx += 1
             continue
-        increaseCharCount(rightCh,subStringChar)
-        if subStringChar[rightCh]==target[rightCh]:
-            numUniqueDone+=1
-        while numUniqueDone==numUnique and leftIdx<=rightIdx:
-            subStringBounds=getCloseBounds(leftIdx,rightIdx,subStringBounds[0],subStringBounds[1])
-            leftCh=string[leftIdx]
+        increaseCharCount(rightCh, subStringChar)
+        if subStringChar[rightCh] == target[rightCh]:
+            numUniqueDone += 1
+        while numUniqueDone == numUnique and leftIdx <= rightIdx:
+            subStringBounds = getCloseBounds(
+                leftIdx, rightIdx, subStringBounds[0], subStringBounds[1])
+            leftCh = string[leftIdx]
             if leftCh not in target:
-                leftIdx+=1
+                leftIdx += 1
                 continue
-            if subStringChar[leftCh]==target[leftCh]:
-                numUniqueDone-=1
-            decreaseCharCount(leftCh,subStringChar)
-            leftIdx+=1
-        rightIdx+=1
+            if subStringChar[leftCh] == target[leftCh]:
+                numUniqueDone -= 1
+            decreaseCharCount(leftCh, subStringChar)
+            leftIdx += 1
+        rightIdx += 1
     return subStringBounds
 
-def getCloseBounds(l,r,subL,subR):
-    if r-l<subR-subL:
-        return [l,r]
+
+def getCloseBounds(l, r, subL, subR):
+    if r-l < subR-subL:
+        return [l, r]
     else:
-        return [subL,subR]
-def getStringFromBounds(string,bounds):
-    start,end=bounds
-    if end==float('inf'):
+        return [subL, subR]
+
+
+def getStringFromBounds(string, bounds):
+    start, end = bounds
+    if end == float('inf'):
         return ''
     return string[start:end+1]
-    
+
+
 def getCharCounts(string):
-    charCounts={}
+    charCounts = {}
     for i in string:
-        increaseCharCount(i,charCounts)
+        increaseCharCount(i, charCounts)
     return charCounts
 
-def increaseCharCount(char,charCounts):
-    if char not in charCounts:
-        charCounts[char]=0
 
-    charCounts[char]+=1
-    
-def decreaseCharCount(char,charCounts):
-    charCounts[char]-=1
-        
+def increaseCharCount(char, charCounts):
+    if char not in charCounts:
+        charCounts[char] = 0
+
+    charCounts[char] += 1
+
+
+def decreaseCharCount(char, charCounts):
+    charCounts[char] -= 1
