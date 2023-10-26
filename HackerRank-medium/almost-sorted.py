9516d6be65f7d493813c437a623106024f941d2b
belisky <denoblesnobility2@gmail.com>
2023-10-26 14:14:30 +0000

    update: adds a second solution to almost sorted

--- HackerRank-medium/almost-sorted.py
+++ HackerRank-medium/almost-sorted.py
@@ -37,3 +37,37 @@ def almostSorted(arr):
         return
     print('no')
     # Write your code here
+
+
+# #solution 2
+# def almostSorted(arr):
+#     arr_sorted = sorted(arr)
+#     if arr_sorted == arr:
+#         print('yes')
+#     elif len(arr) <= 2:
+#         print('yes')
+#         print('swap 1 2')
+#         return
+        
+#     diff = []
+#     for i in range(len(arr)):
+#         if arr[i] != arr_sorted[i]:
+#             diff.append([arr[i], i])
+    
+#     if len(diff) == 2:
+#         print('yes')
+#         print('swap {} {}'.format(diff[0][1] + 1, diff[1][1] + 1))
+        
+#     elif len(diff) < 2:
+#         print('no')
+        
+#     else:
+#         seq = []
+#         for i in diff:
+#             seq.append(i[0])
+#         rev = seq[::-1]
+#         if sorted(rev) == rev:
+#             print('yes')
+#             print('reverse {} {}'.format(diff[0][1] + 1, diff[-1][1] + 1))
+#         else:
+#             print('no')
